9c5a26dd418d5dce62671f796186dfe266deaf44
stuaiki <takaku@byu.edu>
2024-02-28 14:17:21 -0700

    Put some comment

--- Mission08_Team0307/Controllers/HomeController.cs
+++ Mission08_Team0307/Controllers/HomeController.cs
@@ -12,36 +12,43 @@ namespace Mission08_Team0307.Controllers
         {
             _context = temp;
         }
+
+        //return index view
         public IActionResult Index()
         {
             return View();
         }
 
-        [HttpGet]
+        [HttpGet] //default
         public IActionResult TaskForm()
         {
+            // return taskform and create new task entry form
             return View(new TaskEntry());
         }
 
-        [HttpPost]
+
+        [HttpPost] //after post method
         public IActionResult TaskForm(TaskEntry response)
         {
+            //Validation value
             if (ModelState.IsValid)
             {
+                //add new task and save changes
                 _context.Tasks.Add(response);
                 _context.SaveChanges();
 
+                //jump to confirmaiton view and bring values inside response
                 return View("Confirmation", response);
             }
             else
             {
+                //show inputted value and show error message for invalid value
                 return View(response);
             }
         }
 
         public IActionResult Quadrant()
         {
-            
             return View();
         }
     }
--- Mission08_Team0307/Models/TaskEntry.cs
+++ Mission08_Team0307/Models/TaskEntry.cs
@@ -1,23 +1,25 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
+//Create new model called TaskEntry
 namespace Mission08_Team0307.Models
 {
     public class TaskEntry
     {
+        //TaskId is Primary key
         [Key]
-        [Required]
+        [Required] //Required component
         public int TaskId { get; set; }
 
-        [Required(ErrorMessage = "You need to put Task Name")]
+        [Required(ErrorMessage = "You need to put Task Name")] // required component and show message if it's invalid value
         public string Task { get; set; }
 
-        public DateOnly? DueDate { get; set; }
+        public DateOnly? DueDate { get; set; } //nullable value
 
-        [Required(ErrorMessage = "You need to select Quadrant")]
+        [Required(ErrorMessage = "You need to select Quadrant")] // required component and show message if it's invalid value
         public string Quadrant { get; set; }
 
-        public string? Category { get; set; }
+        public string? Category { get; set; } //nullable value
 
-        public bool? Completed { get; set; }
+        public bool? Completed { get; set; } //nullable value
     }
 }
