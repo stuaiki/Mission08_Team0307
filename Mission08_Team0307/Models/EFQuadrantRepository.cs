08e5b817130c0540d0e9408a8b9564ded2a608e8
donnakim03 <dkim210@byu.edu>
2024-03-01 20:54:02 -0700

    donna's comments

--- Mission08_Team0307/Models/EFQuadrantRepository.cs
+++ Mission08_Team0307/Models/EFQuadrantRepository.cs
@@ -10,19 +10,19 @@
 
         public List<TaskEntry> Tasks => _context.Tasks.ToList();
 
-        public void AddTask(TaskEntry task)
+        public void AddTask(TaskEntry task) // method that can be called by _repo to add a task
         {
             _context.Add(task);
             _context.SaveChanges();
         }
 
-        public void DeleteTask(TaskEntry task)
+        public void DeleteTask(TaskEntry task) // method that can be called by _repo to delete a task
         {
             _context.Tasks.Remove(task);
             _context.SaveChanges();
         }
 
-        public void UpdateTask(TaskEntry task)
+        public void UpdateTask(TaskEntry task) // method that can be called by _repo to update task
         {
             _context.Update(task);
             _context.SaveChanges();
